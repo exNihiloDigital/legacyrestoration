d5611da0ab5c708305229aecb37b4c2c423955a4
mrogowski <31445338+miker-phos@users.noreply.github.com>
2019-06-28 13:39:31 -0400

    Updated for github workflow

--- includes/featured.php
+++ includes/featured.php
@@ -1,9 +1,11 @@
 
 <?php // featured('title'), featured('image'), featured('alt'); ?>
-<div style="background-image: url(<?php featured('image') ?>)" class="featured <?php featured('alt') ?>">
-  <div class="container">
-    <h1>
-      <?php featured('title'); ?>
-    </h1>
-  </div>
+<div style="background-image: url(<?php featured('image') ?>)" class="featured overlay <?php featured('alt') ?>">
+  <main>
+    <div>
+      <h1>
+        <?php featured('title') ; ?>
+      </h1>
+    </div>
+  </main>
 </div>
