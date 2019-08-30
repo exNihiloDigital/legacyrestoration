44b14bea07923db562f5aa72ec96c7a16de78042
mrogowski <31445338+miker-phos@users.noreply.github.com>
2019-08-30 13:37:40 -0600

    Removed Double Title Tag

--- header.php
+++ header.php
@@ -4,9 +4,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=10">
   <link type="image/gif" href="<?php echo esc_url(get_theme_file_uri()) ?>/favicon.png" rel="icon" sizes="16x16">
-  <title>
-    <?php wp_title('|', true, 'left'); ?>
-  </title>
   <?php wp_head(); ?>
 </head><body <?php body_class() ?>>
 <div class="mobile">
