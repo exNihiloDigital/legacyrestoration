442363dc7002437f5bce613cb3a70ef8e61813c0
phoshenwen <hendrikw@phoscreative.com>
2021-06-09 09:34:45 -0400

    converted tabs to spaces

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -52,22 +52,22 @@ add_action('wp_enqueue_scripts', 'enqueue_theme');
 function enqueue_theme()
 {
     if (! is_admin()) {
-		// Get the theme data.
-		$the_theme     = wp_get_theme();
-		// Will take the version from root folder /style.css
-		$theme_version = $the_theme->get( 'Version' );
-		// Will append the modified-time to static assets.
-		// theme.min.css becomes theme.min.css?ver=1.0.0195867.
-		// Prevents caching issues as the cache renews with changed file names.
-		// https://www.keycdn.com/support/what-is-cache-busting
+        // Get the theme data.
+        $the_theme     = wp_get_theme();
+        // Will take the version from root folder /style.css
+        $theme_version = $the_theme->get( 'Version' );
+        // Will append the modified-time to static assets.
+        // theme.min.css becomes theme.min.css?ver=1.0.0195867.
+        // Prevents caching issues as the cache renews with changed file names.
+        // https://www.keycdn.com/support/what-is-cache-busting
         $css = file_exists( get_template_directory() . '/assets/css/theme.min.css' ) ? '/assets/css/theme.min.css' : '/style.css';
         $js = file_exists( get_template_directory() . '/assets/js/theme.min.js' ) ? '/assets/js/theme.min.js' : '/style.css';
-		$css_v = $theme_version . '.' . filemtime( get_template_directory() . $css );
-		$js_v = $theme_version . '.' . filemtime( get_template_directory() . $js );
+        $css_v = $theme_version . '.' . filemtime( get_template_directory() . $css );
+        $js_v = $theme_version . '.' . filemtime( get_template_directory() . $js );
 
         wp_enqueue_script('jquery');
         wp_enqueue_script('swiper-js', 'https://unpkg.com/swiper@6.5.8/swiper-bundle.min.js');
-        wp_enqueue_script('theme', get_template_directory_uri() . '/assets/js/theme.min.js', array('jquery'), $js_v, true);
+        wp_enqueue_script('theme', get_template_directory_uri() . '/assets/js/theme.min.js', array('jquery'), $js_v);
 
         wp_enqueue_style('normalize', 'https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css');
         wp_enqueue_style('fontawesome', 'https://pro.fontawesome.com/releases/v5.12.0/css/all.css');
